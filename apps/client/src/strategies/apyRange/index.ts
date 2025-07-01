bcfe927692303c5cccf4fceb32daa8bc81fc9fd1
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2025-07-01 14:03:31 +0200

    fix: idle reallocations

--- apps/client/src/strategies/apyRange/index.ts
+++ apps/client/src/strategies/apyRange/index.ts
@@ -79,14 +79,14 @@ export class ApyRange implements Strategy {
     let idleWithdrawal = 0n;
     let idleDeposit = 0n;
 
-    if (idleMarket && ALLOW_IDLE_REALLOCATION) {
-      if (totalWithdrawableAmount > totalDepositableAmount) {
+    if (idleMarket) {
+      if (totalWithdrawableAmount > totalDepositableAmount && ALLOW_IDLE_REALLOCATION) {
         idleDeposit = min(
           totalWithdrawableAmount - totalDepositableAmount,
           idleMarket.cap - idleMarket.vaultAssets,
         );
         totalDepositableAmount += idleDeposit;
-      } else {
+      } else if (totalDepositableAmount > totalWithdrawableAmount) {
         idleWithdrawal = min(
           totalDepositableAmount - totalWithdrawableAmount,
           idleMarket.vaultAssets,
@@ -145,8 +145,8 @@ export class ApyRange implements Strategy {
       if (remainingWithdrawal === 0n && remainingDeposit === 0n) break;
     }
 
-    if (idleMarket && ALLOW_IDLE_REALLOCATION) {
-      if (idleWithdrawal > 0n) {
+    if (idleMarket) {
+      if (idleWithdrawal > 0n && ALLOW_IDLE_REALLOCATION) {
         withdrawals.push({
           marketParams: idleMarket.params,
           assets: idleWithdrawal,
